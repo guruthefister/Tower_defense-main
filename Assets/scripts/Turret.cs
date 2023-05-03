47ad125a9581f8554c156cd57677adb1d64ce6c0
Guruen <mathiaskrogkaer@gmail.com>
2023-05-03 14:25:14 +0200

    added bullet

--- Assets/scripts/Turret.cs
+++ Assets/scripts/Turret.cs
@@ -5,12 +5,22 @@ public class Turret : MonoBehaviour
 {
 
     private Transform target;
-    public float range = 15f;
+
+    [Header("Attributes")]
+
+    public float range = 15f; 
+    public float fireRate = 1f;
+    private float fireCountdown = 0f;
+
+    [Header("Unity Setup Fields")]
 
     public string enemyTag = "Enemy";
 
     public Transform partToRotate;
     public float turnSpeed = 10f;
+
+    public GameObject bulletPrefab;
+    public Transform firePoint;
 
     void Start()
     {
@@ -52,6 +62,18 @@ public class Turret : MonoBehaviour
         Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
         partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
 
+        if (fireCountdown <= 0f)
+        {
+            Shoot();
+            fireCountdown = 1f / fireRate;
+        }
+
+        fireCountdown -= Time.deltaTime;
+    }
+
+    void Shoot()
+    {
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
     }
 
     private void OnDrawGizmosSelected()
