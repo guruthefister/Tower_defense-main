d341897fc45beee9bc165c537e96d9a268e868ff
Guruen <mathiaskrogkaer@gmail.com>
2023-05-11 15:47:45 +0200

    game2

--- Assets/scripts/Map/GridManager.cs
+++ Assets/scripts/Map/GridManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:81589baa80de3d83e1cc0001507079e592ba8aaf248471fbc426a232fb4c38c4
-size 764
+oid sha256:88a3e36020789f7619079836ddebed1b581c956fafc9017909945b810b12060c
+size 901
--- Assets/scripts/Map/Tile.cs
+++ Assets/scripts/Map/Tile.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:51162ec7ad61df43d121040afa5f06efb25c235d1f970a72e7e04c5adb56485d
-size 306
+oid sha256:fdda63ca0e751f1f2648306debbd4106643dba0af8a0ecc31c38cc8da3f1202b
+size 355
