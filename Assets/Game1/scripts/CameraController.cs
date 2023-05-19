fedf8702063c6c37852449049a825fe8c7435770
Guruen <mathiaskrogkaer@gmail.com>
2023-05-19 15:51:12 +0200

    shop added og ændrede layout af maper

--- Assets/Game1/scripts/CameraController.cs
+++ Assets/Game1/scripts/CameraController.cs
@@ -20,19 +20,19 @@ public class CameraController : MonoBehaviour {
         if (!doMovement)
             return;
         
-        if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness) 
+        if (Input.GetKey("w") )//|| Input.mousePosition.y >= Screen.height - panBorderThickness) 
         {
             transform.Translate(Vector3.back/*would normally be Vector3.forward, but messed up camera in unity itself*/ * panSpeed * Time.deltaTime, Space.World);
         }
-        if (Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness) 
+        if (Input.GetKey("s") )//|| Input.mousePosition.y <= panBorderThickness) 
         {
             transform.Translate(Vector3.forward/*would normally be Vector3.back, but messed up camera in unity itself*/ * panSpeed * Time.deltaTime, Space.World);
         }
-        if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness) 
+        if (Input.GetKey("d") )//|| Input.mousePosition.x >= Screen.width - panBorderThickness) 
         {
             transform.Translate(Vector3.left/*would normally be Vector3.right, but messed up camera in unity itself*/ * panSpeed * Time.deltaTime, Space.World);
         }
-        if (Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness) 
+        if (Input.GetKey("a") )//|| Input.mousePosition.x <= panBorderThickness) 
         {
             transform.Translate(Vector3.right/*would normally be Vector3.left, but messed up camera in unity itself*/ * panSpeed * Time.deltaTime, Space.World);
         }
