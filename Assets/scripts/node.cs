45be4e1a8bc9f7e963a234c886eaf4c37991cd36
Guruen <mathiaskrogkaer@gmail.com>
2023-06-12 10:56:41 +0200

    Revert "shop added og ændrede layout af maper"
    This reverts commit fedf8702063c6c37852449049a825fe8c7435770.

--- Assets/scripts/node.cs
+++ Assets/scripts/node.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using UnityEngine.EventSystems;
 
 public class node : MonoBehaviour {
 
@@ -11,42 +10,26 @@ public class node : MonoBehaviour {
     private Renderer rend;
     private Color startColor; 
 
-    BuildManager buildManager;
-
     void Start () 
     {
         rend = GetComponent<Renderer>();
         startColor = rend.material.color;
-
-        buildManager = BuildManager.instance;
     }
 
     void OnMouseDown () 
     {
-        if (EventSystem.current.IsPointerOverGameObject())
-            return;
-
-        if (buildManager.GetTurretToBuild() == null)
-            return;
-
         if (turret != null)
         {
             Debug.Log("Can't build there! - TODO: Display on screen.");
             return;
         }
 
-        GameObject turretToBuild = buildManager.GetTurretToBuild();
+        GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
         turret = (GameObject)Instantiate(turretToBuild, transform.position + positionOffset, transform.rotation);
     }
 
     void OnMouseEnter ()
     {
-        if (EventSystem.current.IsPointerOverGameObject())
-            return;
-
-        if (buildManager.GetTurretToBuild() == null)
-            return;
-
         rend.material.color = hoverColor;
     }
 
