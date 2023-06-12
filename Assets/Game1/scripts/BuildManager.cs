dd194b9369d1d6248b16841ca2fb2dcee36e9a00
Guruen <mathiaskrogkaer@gmail.com>
2023-06-12 10:59:10 +0200

    Revert "Revert "shop added og ændrede layout af maper""
    This reverts commit 45be4e1a8bc9f7e963a234c886eaf4c37991cd36.

--- Assets/Game1/scripts/BuildManager.cs
+++ Assets/Game1/scripts/BuildManager.cs
@@ -15,17 +15,18 @@ public class BuildManager : MonoBehaviour {
     }
 
     public GameObject standardTurretPrefab;
+    public GameObject anotherTurretPrefab;
 
-    void Start ()
-    {
-        turretToBuild = standardTurretPrefab;
-    }
-    
     private GameObject turretToBuild;
 
     public GameObject GetTurretToBuild ()
     {
         return turretToBuild;
+    }
+
+    public void SetTurretToBuild (GameObject turret)
+    {
+        turretToBuild = turret;
     }
 
 }
